71c78d7f78cd85361f9ace2a7ad240245ebe15f7
NabeelSaleem333 <nabeel.saleem333@gmail.com>
2020-09-10 18:47:20 +0500

    get method is add

--- src/api/resources/users/user.controller.js
+++ src/api/resources/users/user.controller.js
@@ -10,3 +10,14 @@ exports.save = async (req, res) => {
     return res.json(error);
   }
 };
+//
+exports.find = async (req, res) => {
+    try {
+    //   console.log("Users req.body: ", JSON.parse(JSON.stringify(req.body)));
+      //
+      const user = await Users.find();
+      res.json({ success: true, status: "User saved success!", user: user });
+    } catch (error) {
+      return res.json(error);
+    }
+  };
