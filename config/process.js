2d2c76ccb0420fb83a32ff5bf006d79867db3ee3
NabeelSaleem333 <nabeel.saleem333@gmail.com>
2020-09-11 12:23:23 +0500

    db connection changed

--- config/process.js
+++ config/process.js
@@ -1,6 +1,9 @@
 const devConfig = {
 PORT: process.env.PORT || 1000,
-MongoCon : 'mongodb://localhost/myasasa',
+MongoCon : 'mongodb+srv://medical:express@medicalexpresscluster-hkv5p.mongodb.net/myasasa?retryWrites=true&w=majority',
 secretKey: 123321
 };
 module.exports = devConfig;
+
+// mongodb+srv://medical:express@medicalexpresscluster-hkv5p.mongodb.net/medicalexpress?retryWrites=true&w=majority
+// mongodb://localhost/myasasa 
