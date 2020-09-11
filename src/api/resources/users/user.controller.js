34743730af96c4c5a99b35ffef168e3b938d9dde
NabeelSaleem333 <nabeel.saleem333@gmail.com>
2020-09-11 12:41:49 +0500

    find method updated

--- src/api/resources/users/user.controller.js
+++ src/api/resources/users/user.controller.js
@@ -5,19 +5,27 @@ exports.save = async (req, res) => {
     console.log("Users req.body: ", JSON.parse(JSON.stringify(req.body)));
     //
     const user = await Users.create(req.body);
-    res.json({ success: true, status: "User saved success!", user: user });
+    return res.json({
+      success: true,
+      status: "User saved success!",
+      user: user,
+    });
   } catch (error) {
     return res.json(error);
   }
 };
 //
 exports.find = async (req, res) => {
-    try {
+  try {
     //   console.log("Users req.body: ", JSON.parse(JSON.stringify(req.body)));
-      //
-      const user = await Users.find();
-      res.json({ success: true, status: "User saved success!", user: user });
-    } catch (error) {
-      return res.json(error);
-    }
-  };
+    //
+    // const user = await Users.find();
+    return res.json({
+      success: true,
+      status: "User saved success!",
+    //   user: user,
+    });
+  } catch (error) {
+    return res.json(error);
+  }
+};
